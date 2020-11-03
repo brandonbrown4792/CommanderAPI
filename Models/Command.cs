257f322040916ace4aa3f22abe3ad8736cd4038f
Brandon Brown <brandonbrown4792@gmail.com>
2020-11-03 15:44:29 -0500

    Added migrations

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Models
 {
     public class Command
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required] // Means cannot be null in database
+        [MaxLength(250)] // Sets string max length to 250 characters
         public string HowTo { get; set; }
+
+        [Required]
         public string Line { get; set; }
+
+        [Required]
         public string Platform { get; set; }
     }
 }
